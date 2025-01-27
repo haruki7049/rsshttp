e8617cf847cfa2bca78e04fd12470501d9d9e4ce
haruki7049 <tontonkirikiri@gmail.com>
2025-01-27 21:28:59 +0900

    chore(logging): Add tracing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let app = Router::new().route("/", get(root));
+    tracing_subscriber::fmt::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
 
+    let app = Router::new().route("/", get(root));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
+    tracing::info!("Server started at 0.0.0.0:3000");
     axum::serve(listener, app).await?;
 
     Ok(())
 }
 
+#[tracing::instrument]
 async fn root() -> &'static str {
     "Hello, World!!\n"
 }
