cce2f743dd128a173aba9f19516a05ca5fc8bb00
haruki7049 <tontonkirikiri@gmail.com>
2025-01-27 19:04:31 +0900

    feat(src/main.rs): Init minimal and simple HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> std::io::Result<()> {
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, World!!\n"
 }
