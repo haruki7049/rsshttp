145c751f47ad49cbe70fa7d5ea029f33205f6a5c
haruki7049 <tontonkirikiri@gmail.com>
2025-01-28 12:13:16 +0900

    Add some comments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // This section reads RUST_LOG env
     tracing_subscriber::fmt::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::builder()
@@ -11,9 +12,11 @@ async fn main() -> std::io::Result<()> {
         .json()
         .init();
 
+    // Creating app & listener
     let app = Router::new().route("/", get(root));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
+    // Running server
     tracing::info!("Server started at 0.0.0.0:3000");
     axum::serve(listener, app).await?;
 
